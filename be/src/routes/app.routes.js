55b3c869116fdaa312e92b33bc438733da898892
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2026-04-02 10:42:15 +0530

    add : auth global route is added

--- be/src/routes/app.routes.js
+++ be/src/routes/app.routes.js
@@ -2,13 +2,17 @@ import { Router } from "express";
 import userRouter from "./user.routes.js"
 import captainRouter from './captain.routes.js'
 import systemRouter from './system.routes.js'
+import authRouter from './auth.routes.js'
 const router=Router();
 
 //user routes
 router.use("/user",userRouter)
 //captain routes
-router.use("/captain",userRouter)
+router.use("/captain",captainRouter)
 //system routes
 router.use("/system",systemRouter)
+
+//auth routes
+router.use("/auth",authRouter)
 
 export default router
